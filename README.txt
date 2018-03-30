a76fbd83bd8ae7ee0c8dec28ec69c77fe82fcc90
Cezanne <cezannec@alumni.stanford.edu>
2018-03-29 17:52:18 -0700

    Initial project commit

---
+++ README.txt
@@ -0,0 +1,31 @@
+Rubric:
+
+Listed below are the requirements to pass this project.
+
+* In models.py, specify the CNN architecture; define a convolutional neural network with at least one convolutional layer, i.e. self.conv1 = nn.Conv2d(1, 32, 3).
+
+* In notebook 2, you should train the network after defining its loss and optimization functions. You are encouraged, but not required, to visualize their loss over time/epochs by printing it out occasionally and/or plotting the loss over time.
+
+* After training, all questions in notebook 2 about model architecture and choice of loss function are answered.
+
+* In notebook 2, visualize one or more learned feature maps
+
+* In notebook 3, use a haar cascade face detector to detect faces in a given image
+
+* In notebook 3, process each image of a face into the shape your model from notebook 2 expects
+
+* In notebook 3, apply your trained network to the image and display detected key points on a face.
+
+
+
+For testers:
+
+Please answer the following questions, as I'm aimiing to improve and add to this project:
+
+* How could this project be improved?
+* Where were the instructions most confusing?
+
+* Would you like the ability to overlay simple graphics on a face -- i.e sunglasses on detected eye keypoints?
+* Would you like to learn how to group faces by pose (facing left/right/etc) using unsupervised clustering like k-means?
+
+Thank you so much for your time!
---
+++ models.py
@@ -0,0 +1,35 @@
+## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and layers to avoid overfitting
+        
+
+        
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
