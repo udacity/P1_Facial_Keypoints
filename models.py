461bf5d4fa814bb783f2d2d96c83f22e0514535d
Cezanne Camacho <cezannec@alumni.stanford.edu>
2018-04-11 18:20:14 -0700

    Update imports

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
 
 
 class Net(nn.Module):
